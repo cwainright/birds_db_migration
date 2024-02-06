79bdc7a0881801e9c27a85687a89bc0b6ccd93ad
cwainright-nps <charles_wainright@nps.gov>
2024-02-06 17:10:39 -0500

    refactor template list as a constant in `assets`, add save-to-pickle feature, update crosswalk for `DetectionEvent` table

--- src/make_templates.py
+++ src/make_templates.py
@@ -6,10 +6,8 @@ import pickle
 import src.build_tbls as bt
 import numpy as np
 
-template_list = [ # add destination tables here as we build them
-    'ncrn.DetectionEvent'
-    # ,'ncrn.BirdDetection'
-    ]
+# template_list = assets.DEST_LIST.copy()
+template_list = list(assets.TBL_XWALK.values())
 
 def _make_templates(dest:str='assets/templates/templates.pkl', template_list:list=template_list) -> dict:
     """Make empty dataframes with the correct column names and order for each table to be loaded
@@ -43,8 +41,8 @@ def _make_templates(dest:str='assets/templates/templates.pkl', template_list:lis
         except:
             print(f'There is no table {tbl}')
     
-    with open(dest, 'wb') as f:
-        pickle.dump(template_dict, f)
+    # with open(dest, 'wb') as f:
+    #     pickle.dump(template_dict, f)
 
     # SQL_QUERY = f"""SELECT TOP 5 * FROM [{assets.LOC_DB}].[dbo].[ncrn.BirdDetection];"""
     # df = pd.read_sql_query(SQL_QUERY,con)
@@ -55,24 +53,25 @@ def _make_templates(dest:str='assets/templates/templates.pkl', template_list:lis
     return template_dict
 
 
-def make_xwalks(dest:str='assets/templates/xwalks.pkl', template_list:list=template_list) -> dict:
+def make_xwalks(dest:str='') -> dict:
     """Create a dictionary of crosswalks for each table in the source (Access) and destination (SQL Server) databases
 
     Args:
-        dest (str, optional): _description_. Defaults to 'assets/templates/xwalks.pkl'.
-        template_list (list, optional): _description_. Defaults to `template_list`.
+        dest (str, optional): Relative or absolute filepath to which a pickle of the output should be saved. Must end in '.pkl'. Defaults to ''.
 
     Returns:
-        dict: a dictionary 
+        dict: a containing destination dataframes and the source componenets from which they were generated 
 
     Examples:
         import src.make_templates as mt
-        testdict = mt.make_xwalks()
+        testdict = mt.make_xwalks('saved_dictionary.pkl')
         with open('saved_dictionary.pkl', 'rb') as f:
             loaded_dict = pickle.load(f) 
     """
+    if dest !='':
+        assert dest.endswith('.pkl'), print(f'You entered `{dest}`. `dest` must end in ".pkl"')
 
-    source_dict = bt.build_tbl()
+    source_dict = bt._get_tbls()
     dest_dict = _make_templates()
 
     # create empty structure to receive data
@@ -80,29 +79,36 @@ def make_xwalks(dest:str='assets/templates/xwalks.pkl', template_list:list=templ
     for tbl in template_list:
         xwalk_dict[tbl] = {
             'xwalk': pd.DataFrame(columns=['source', 'destination'])
-            ,'source': {}
+            ,'source': pd.DataFrame()
+            ,'source_name': ''
             ,'destination': dest_dict[tbl]
         }
 
     # add xwalk to empty structure for each destination table
     xwalk_dict = _detection_event_xwalk(xwalk_dict)
+    # xwalk_dict = _bird_detection_xwalk(xwalk_dict)
 
     # add source dataframe to structure for each destination table
     for tbl in template_list:
-        source_tbl = list(xwalk_dict[tbl]['source'].keys())[0]
+        source_tbl = xwalk_dict[tbl]['source_name']
         try:
             print(source_tbl)
-            xwalk_dict[tbl]['source'][source_tbl] = source_dict[source_tbl]
+            xwalk_dict[tbl]['source'] = source_dict[source_tbl]
         except:
             pass
 
     # xwalk each source dataframe to destination structure, according to xwalk
     
+    # save output if desired
+    if dest !='':
+        with open(dest, 'wb') as f:
+            pickle.dump(xwalk_dict, f)
+        print(f'Output saved to `{dest}`')
     
     return xwalk_dict
 
 def _detection_event_xwalk(xwalk_dict:dict) -> dict:
-    """Crosswalk source.tbl_field_data to destination.ncrn.BirdDetection
+    """Crosswalk source.tbl_Events to destination.ncrn.DetectionEvent
 
     Args:
         xwalk_dict (dict): dictionary of column names crosswalked between source and destination tables
@@ -111,21 +117,33 @@ def _detection_event_xwalk(xwalk_dict:dict) -> dict:
         dict: dictionary of column names crosswalked between source and destination tables with data updated for this table
     """
     
-    xwalk_dict['ncrn.DetectionEvent']['source'] = {'tbl_Events': pd.DataFrame()}
+    xwalk_dict['ncrn.DetectionEvent']['source_name'] = 'tbl_Events'
     xwalk_dict['ncrn.DetectionEvent']['xwalk']['destination'] = xwalk_dict['ncrn.DetectionEvent']['destination'].columns
     mask = (xwalk_dict['ncrn.DetectionEvent']['xwalk']['destination'] == 'ID')
-    xwalk_dict['ncrn.DetectionEvent']['xwalk']['source'] =  np.where(mask, 'Event_ID', xwalk_dict['ncrn.DetectionEvent']['xwalk']['source'])
+    xwalk_dict['ncrn.DetectionEvent']['xwalk']['source'] =  np.where(mask, 'event_id', xwalk_dict['ncrn.DetectionEvent']['xwalk']['source'])
     mask = (xwalk_dict['ncrn.DetectionEvent']['xwalk']['destination'] == 'LocationID')
-    xwalk_dict['ncrn.DetectionEvent']['xwalk']['source'] =  np.where(mask, 'Location_ID', xwalk_dict['ncrn.DetectionEvent']['xwalk']['source'])
+    xwalk_dict['ncrn.DetectionEvent']['xwalk']['source'] =  np.where(mask, 'location_id', xwalk_dict['ncrn.DetectionEvent']['xwalk']['source'])
     mask = (xwalk_dict['ncrn.DetectionEvent']['xwalk']['destination'] == 'ProtocolID')
-    xwalk_dict['ncrn.DetectionEvent']['xwalk']['source'] =  np.where(mask, 'Protocol_ID', xwalk_dict['ncrn.DetectionEvent']['xwalk']['source'])
-    mask = (xwalk_dict['ncrn.DetectionEvent']['xwalk']['destination'] == 'ProtocolID')
-    xwalk_dict['ncrn.DetectionEvent']['xwalk']['source'] =  np.where(mask, 'Protocol_ID', xwalk_dict['ncrn.DetectionEvent']['xwalk']['source'])
+    xwalk_dict['ncrn.DetectionEvent']['xwalk']['source'] =  np.where(mask, 'protocol_id', xwalk_dict['ncrn.DetectionEvent']['xwalk']['source'])
+    mask = (xwalk_dict['ncrn.DetectionEvent']['xwalk']['destination'] == 'EnteredBy')
+    xwalk_dict['ncrn.DetectionEvent']['xwalk']['source'] =  np.where(mask, 'entered_by', xwalk_dict['ncrn.DetectionEvent']['xwalk']['source'])
+    mask = (xwalk_dict['ncrn.DetectionEvent']['xwalk']['destination'] == 'AirTemperature')
+    xwalk_dict['ncrn.DetectionEvent']['xwalk']['source'] =  np.where(mask, 'temperature', xwalk_dict['ncrn.DetectionEvent']['xwalk']['source'])
+    mask = (xwalk_dict['ncrn.DetectionEvent']['xwalk']['destination'] == 'Notes')
+    xwalk_dict['ncrn.DetectionEvent']['xwalk']['source'] =  np.where(mask, 'event_notes', xwalk_dict['ncrn.DetectionEvent']['xwalk']['source'])
+    mask = (xwalk_dict['ncrn.DetectionEvent']['xwalk']['destination'] == 'EnteredDateTime')
+    xwalk_dict['ncrn.DetectionEvent']['xwalk']['source'] =  np.where(mask, 'entered_date', xwalk_dict['ncrn.DetectionEvent']['xwalk']['source'])
+    mask = (xwalk_dict['ncrn.DetectionEvent']['xwalk']['destination'] == 'DataProcessingLevelID')
+    xwalk_dict['ncrn.DetectionEvent']['xwalk']['source'] =  np.where(mask, 'dataprocessinglevelid', xwalk_dict['ncrn.DetectionEvent']['xwalk']['source'])
+    mask = (xwalk_dict['ncrn.DetectionEvent']['xwalk']['destination'] == 'DataProcessingLevelDate')
+    xwalk_dict['ncrn.DetectionEvent']['xwalk']['source'] =  np.where(mask, 'dataprocessingleveldate', xwalk_dict['ncrn.DetectionEvent']['xwalk']['source'])
+    mask = (xwalk_dict['ncrn.DetectionEvent']['xwalk']['destination'] == 'RelativeHumidity')
+    xwalk_dict['ncrn.DetectionEvent']['xwalk']['source'] =  np.where(mask, 'humidity', xwalk_dict['ncrn.DetectionEvent']['xwalk']['source'])
 
     return xwalk_dict
 
 def _bird_detection_xwalk(xwalk_dict:dict) -> dict:
-    """Crosswalk source.tbl_Event to destination.ncrn.DetectionEvent
+    """Crosswalk source.tbl_Event to destination.ncrn.BirdDetection
 
     Args:
         xwalk_dict (dict): dictionary of column names crosswalked between source and destination tables
@@ -133,7 +151,7 @@ def _bird_detection_xwalk(xwalk_dict:dict) -> dict:
     Returns:
         dict: dictionary of column names crosswalked between source and destination tables with data updated for this table
     """
-    xwalk_dict['ncrn.DetectionEvent']['source_tbl_name'] = 'tbl_Field_Data'
+    xwalk_dict['ncrn.DetectionEvent']['source_name'] = 'tbl_Field_Data'
     xwalk_dict['ncrn.DetectionEvent']['xwalk']
 
     return xwalk_dict
