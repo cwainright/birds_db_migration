4f04056aa7956643de8b62942fd3fb629edb6868
cwainright-nps <charles_wainright@nps.gov>
2024-02-07 15:38:56 -0500

    documented workflow to main

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+"""
+WORKFLOW -- repeat these steps for each destination table to scale up
+1. add key-value pair to assets.assets.TBL_XWALK
+    e.g., 'ncrn.DetectionEvent':'tbl_Events'
+2. add source (Access) query to src.qry
+    - query should match table name exactly and include all columns that need to be xwalked
+    e.g., to query 'tbl_Events', the query must be named 'get_tbl_Events.sql' and the value in assets.assets.TBL_XWALK must be 'tbl_Events'
+3. add xwalk function to src.table_xwalks.py
+    e.g., _detection_event_xwalk()
+4. add a function call to src.make_templates.create_xwalks() for the function you wrote in step 3
+"""
+
 import src.make_templates as mt
 
 testdict = mt.make_xwalks()
