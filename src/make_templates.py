aaf710901e743ad68724bbaf55c23155895ceff8
cwainright-nps <charles_wainright@nps.gov>
2024-02-07 15:40:25 -0500

    drafted logic to execute xwalks, refactored individual table xwalks to src.tbl_xwalks, added TODO functions for validating outputs

--- src/make_templates.py
+++ src/make_templates.py
@@ -3,6 +3,7 @@ import assets.assets as assets
 import pandas as pd
 import pickle
 import src.build_tbls as bt
+import src.tbl_xwalks as tw
 import numpy as np
 
 # template_list = assets.DEST_LIST.copy()
@@ -29,28 +30,31 @@ def make_xwalks(dest:str='') -> dict:
     source_dict = bt._get_src_tbls() # query the source data (i.e., the Access table(s) containing fields)
     dest_dict = bt._get_dest_tbls() # query the destination data (i.e., the SQL Server table; usually an empty dataframe with the correct columns)
 
-    # object to hold data
+    # main object to hold data
     xwalk_dict = {}
     for tbl in template_list:
         xwalk_dict[tbl] = {
-            'xwalk': pd.DataFrame(columns=['source', 'destination', 'calculation']) # document the crosswalk for each table
+            'xwalk': pd.DataFrame(columns=['destination', 'source', 'calculation', 'note']) # document the crosswalk for each table
             ,'source_name': assets.TBL_XWALK[tbl] # store the name of the source table
             ,'source': pd.DataFrame() # placeholder to store the source data
             ,'destination': dest_dict[tbl] # store the destination data (mostly just for its column names and order)
             ,'tbl_load': pd.DataFrame() # placeholder for the source data crosswalked to the destination schema
         }
         xwalk_dict[tbl]['source'] = source_dict[xwalk_dict[tbl]['source_name']] # route the source data to its placeholder
+        xwalk_dict[tbl]['tbl_load'] = pd.DataFrame(columns=xwalk_dict[tbl]['destination'].columns)
         xwalk_dict[tbl]['xwalk']['destination'] = xwalk_dict[tbl]['destination'].columns # route the destination columns to their placeholder in the crosswalk
 
     # create xwalk for each destination table
     xwalk_dict = _create_xwalks(xwalk_dict)
 
+    # validate
+    xwalk_dict = _validate_xwalks(xwalk_dict)
+
     # execute xwalk to generate load
     xwalk_dict = _execute_xwalks(xwalk_dict)
 
     # validate
-    # TODO: write logic to check that each column we hard-coded into ['xwalk']['source'] and ['xwalk']['destination'] actually exists in the dataframe so we can't go sideways on column reassignment
-    xwalk_dict = _validate_xwalks(xwalk_dict)
+    xwalk_dict = _validate_tbl_loads(xwalk_dict)
 
     # save output
     if dest !='':
@@ -62,22 +66,55 @@ def make_xwalks(dest:str='') -> dict:
 
 def _create_xwalks(xwalk_dict:dict) -> dict:
 
-    xwalk_dict = _detection_event_xwalk(xwalk_dict)
-    xwalk_dict = _bird_detection_xwalk(xwalk_dict)
+    xwalk_dict = tw._detection_event_xwalk(xwalk_dict)
+    xwalk_dict = tw._bird_detection_xwalk(xwalk_dict)
 
     return xwalk_dict
 
 def _execute_xwalks(xwalk_dict:dict) -> dict:
     # TODO: this function should execute the instructions stored in each table's `xwalk` to produce a `tbl_load`
+
+    for tbl in template_list:
+        print(tbl)
+        xwalk = xwalk_dict[tbl]['xwalk']
+        # if destination has a one-to-one source field, execute assignments
+        one_to_ones = list(xwalk[xwalk['calculation']=='map_source_to_destination_1_to_1'].destination.values)
+        for dest_col in one_to_ones:
+            print(dest_col)
+            src_col = xwalk[xwalk['destination']==dest_col].source.values[0]
+            print(src_col)
+            xwalk_dict[tbl]['tbl_load'][dest_col] = xwalk_dict[tbl]['source'][src_col]
+
+        # if it's a calculate field, calculate
+        # calculates = list(xwalk[xwalk['calculation']=='calculate_dest_field_from_source_field'].destination.values)
+        # if it's a blank field, leave blank
+        # blanks = list(xwalk[xwalk['calculation']=='blank_field'].destination.values)
+
     return xwalk_dict
 
-def _validate_xwalks(xwalk_dict:dict) -> dict:
+def _validate_tbl_loads(xwalk_dict:dict) -> dict:
+    # TODO: this function should check that the `tbl_load` attr produced from each `source` and `xwalk` is valid
+
     # check that dims of `tbl_load` == dims of `source` (same number of rows and columns)
     xwalk_dict = _validate_dims(xwalk_dict)
     # check that each column in `tbl_load` exists in `destination`
     # check that the column order in `tbl_load` matches that of `destination`
     xwalk_dict = _validate_cols(xwalk_dict)
     # check constraints? may be more work than simply letting sqlserver do the checks
+    return xwalk_dict
+
+
+def _validate_xwalks(xwalk_dict:dict) -> dict:
+    # TODO: this function should check that the `xwalk` attr produced for each `tbl_load` is valid
+
+    # find and report missing values
+    missing = xwalk_dict['ncrn.DetectionEvent']['xwalk'][xwalk_dict['ncrn.DetectionEvent']['xwalk']['source'].isna()].destination.unique()
+    if len(missing) >0:
+        for m in missing:
+            print(f'[\'ncrn.DetectionEvent\'][\'xwalk\'] is missing a `source` value where `destination`==\'{m}\'.')
+
+    # how else can the xwalk go sideways?
+
     return xwalk_dict
 
 def _validate_dims(xwalk_dict:dict) -> dict:
@@ -89,85 +126,4 @@ def _validate_cols(xwalk_dict:dict) -> dict:
     # TODO: check that the column order in `tbl_load` matches that of `destination`
     return xwalk_dict
 
-def _detection_event_xwalk(xwalk_dict:dict) -> dict:
-    """Make the crosswalk for source.tbl_Events to destination.ncrn.DetectionEvent
 
-    Crosswalk collects three types of information:
-    1. 1:1 fields. When a destination field has a matching source field, `source` captures the source field name and `calculation` is '1_to_1_fieldmap'.
-    2. Calculated fields. When one or more source fields need to be combined to match the destination field, `source` captures the code to manipulate and `calculation` is 'calculate'.
-    3. Blanks. When a destination field is not required and has no matching source field, `source` is np.NaN and `calculation` is 'blank'.
-
-    Args:
-        xwalk_dict (dict): dictionary of column names crosswalked between source and destination tables
-
-    Returns:
-        dict: dictionary of column names crosswalked between source and destination tables with data updated for this table
-    """
-    
-    # 1:1 fields
-    mask = (xwalk_dict['ncrn.DetectionEvent']['xwalk']['destination'] == 'ID')
-    xwalk_dict['ncrn.DetectionEvent']['xwalk']['source'] =  np.where(mask, 'event_id', xwalk_dict['ncrn.DetectionEvent']['xwalk']['source'])
-    mask = (xwalk_dict['ncrn.DetectionEvent']['xwalk']['destination'] == 'LocationID')
-    xwalk_dict['ncrn.DetectionEvent']['xwalk']['source'] =  np.where(mask, 'location_id', xwalk_dict['ncrn.DetectionEvent']['xwalk']['source'])
-    mask = (xwalk_dict['ncrn.DetectionEvent']['xwalk']['destination'] == 'ProtocolID')
-    xwalk_dict['ncrn.DetectionEvent']['xwalk']['source'] =  np.where(mask, 'protocol_id', xwalk_dict['ncrn.DetectionEvent']['xwalk']['source'])
-    mask = (xwalk_dict['ncrn.DetectionEvent']['xwalk']['destination'] == 'EnteredBy')
-    xwalk_dict['ncrn.DetectionEvent']['xwalk']['source'] =  np.where(mask, 'entered_by', xwalk_dict['ncrn.DetectionEvent']['xwalk']['source'])
-    mask = (xwalk_dict['ncrn.DetectionEvent']['xwalk']['destination'] == 'AirTemperature')
-    xwalk_dict['ncrn.DetectionEvent']['xwalk']['source'] =  np.where(mask, 'temperature', xwalk_dict['ncrn.DetectionEvent']['xwalk']['source'])
-    mask = (xwalk_dict['ncrn.DetectionEvent']['xwalk']['destination'] == 'Notes')
-    xwalk_dict['ncrn.DetectionEvent']['xwalk']['source'] =  np.where(mask, 'event_notes', xwalk_dict['ncrn.DetectionEvent']['xwalk']['source'])
-    mask = (xwalk_dict['ncrn.DetectionEvent']['xwalk']['destination'] == 'EnteredDateTime')
-    xwalk_dict['ncrn.DetectionEvent']['xwalk']['source'] =  np.where(mask, 'entered_date', xwalk_dict['ncrn.DetectionEvent']['xwalk']['source'])
-    mask = (xwalk_dict['ncrn.DetectionEvent']['xwalk']['destination'] == 'DataProcessingLevelID')
-    xwalk_dict['ncrn.DetectionEvent']['xwalk']['source'] =  np.where(mask, 'dataprocessinglevelid', xwalk_dict['ncrn.DetectionEvent']['xwalk']['source'])
-    mask = (xwalk_dict['ncrn.DetectionEvent']['xwalk']['destination'] == 'DataProcessingLevelDate')
-    xwalk_dict['ncrn.DetectionEvent']['xwalk']['source'] =  np.where(mask, 'dataprocessingleveldate', xwalk_dict['ncrn.DetectionEvent']['xwalk']['source'])
-    mask = (xwalk_dict['ncrn.DetectionEvent']['xwalk']['destination'] == 'RelativeHumidity')
-    xwalk_dict['ncrn.DetectionEvent']['xwalk']['source'] =  np.where(mask, 'humidity', xwalk_dict['ncrn.DetectionEvent']['xwalk']['source'])
-    mask = (xwalk_dict['ncrn.DetectionEvent']['xwalk']['source'].isna()==False)
-    xwalk_dict['ncrn.DetectionEvent']['xwalk']['calculation'] =  np.where(mask, '1_to_1_fieldmap', xwalk_dict['ncrn.DetectionEvent']['xwalk']['calculation'])
-
-    # Calculated fields
-    # `AirTemperatureRecorded` BIT type (bool as 0 or 1); not a NCRN field. Should be 1 when the row has a value in `AirTemperature`
-    # `StartDateTime` need to make datetime from src_tbl.Date and src_tbl.Start_Time
-    # `IsSampled` BIT type (bool as 0 or 1) I think this is the inverse of src_tbl.is_excluded, which is a boolean in Access with one unique value: [0]
-    # -- lookup-constrained calculations
-    # `SamplingMethodID`: [lu.SamplingMethod]([ID])
-    # `Observer_ContactID`: [ncrn.Contact]([ID])
-    # `Recorder_ContactID`: [ncrn.Contact]([ID])
-    # `Observer_ExperienceLevelID`: [lu.ExperienceLevel]([ID])
-    # `ProtocolNoiseLevelID`: [lu.NoiseLevel]([ID])
-    # `ProtocolWindCodeID`: [lu.WindCode]([ID])
-    # `ProtocolPrecipitationTypeID`: [lu.PrecipitationType]([ID])
-    # `TemperatureUnitCode`: [lu.TemperatureUnit]([ID])
-
-    # Blanks
-    # `DataProcessingLevelNote`
-    # `Rowversion`
-    # `UserCode`
-    blank_fields = ['DataProcessingLevelNote', 'Rowversion','UserCode']
-    mask = (xwalk_dict['ncrn.DetectionEvent']['xwalk']['source'].isin(blank_fields))
-    xwalk_dict['ncrn.DetectionEvent']['xwalk']['calculation'] =  np.where(mask, 'leave_blank', xwalk_dict['ncrn.DetectionEvent']['xwalk']['calculation'])
-
-    # validate
-    missing = xwalk_dict['ncrn.DetectionEvent']['xwalk'][xwalk_dict['ncrn.DetectionEvent']['xwalk']['source'].isna()].destination.unique()
-    if len(missing) >0:
-        for m in missing:
-            print(f'[\'ncrn.DetectionEvent\'][\'xwalk\'] is missing a `source` value where `destination`==\'{m}\'.')
-
-    return xwalk_dict
-
-def _bird_detection_xwalk(xwalk_dict:dict) -> dict:
-    """Crosswalk source.tbl_Event to destination.ncrn.BirdDetection
-
-    Args:
-        xwalk_dict (dict): dictionary of column names crosswalked between source and destination tables
-
-    Returns:
-        dict: dictionary of column names crosswalked between source and destination tables with data updated for this table
-    """
-    # xwalk_dict['ncrn.BirdDetection']['source_name'] = 'tbl_Field_Data'
-    # xwalk_dict['ncrn.DetectionEvent']['xwalk']
-
-    return xwalk_dict
