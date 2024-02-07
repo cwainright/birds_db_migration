98b7dea72f9acfa65c28c00d15228b956a99d5b4
cwainright-nps <charles_wainright@nps.gov>
2024-02-07 10:38:03 -0500

    refactored to leverage src.db_connect module

--- src/build_tbls.py
+++ src/build_tbls.py
@@ -1,49 +1,56 @@
-"""Build SQL server (i.e., destination) tables from Access (i.e., source) tables""" 
+"""build dataframes by querying databases (destination==SQL Server; source==Access)""" 
 import pandas as pd
-import numpy as np
 import src.db_connect as dbc
 import assets.assets as assets
 
 tbl_list = list(assets.TBL_XWALK.values())
+template_list = list(assets.TBL_XWALK.keys())
 
-def build_detection_event() -> pd.DataFrame:
-    # connect to db
-    con = dbc._db_connect('access')
-    # execute qry against db
-    df = dbc._exec_qry(con=con, qry='get_tbl_events')
+def _get_dest_tbls(dest:str='assets/templates/templates.pkl', template_list:list=template_list) -> dict:
+    """Make empty dataframes with the correct column names and order for each table to be loaded
+
+    Args:
+        dest (str, optional): Absolute or relative filepath where you want to save the dictionary of dataframes
+
+    Returns:
+        dict: Dictionary of dataframes
+
+    Examples:
+        import src.make_templates as bt
+        testdict = bt._get_dest_tbls()
+        with open('saved_dictionary.pkl', 'rb') as f:
+            loaded_dict = pickle.load(f)
+    """
+    con = dbc._db_connect('local')
+    template_dict = {}
+    for tbl in template_list:
+        SQL_QUERY = f"""SELECT TOP 5 * FROM [{assets.LOC_DB}].[dbo].[{tbl}];"""
+        template_dict[tbl] = pd.read_sql_query(SQL_QUERY,con)
+    
     # close connection
     con.close()
-    # preprocess
-    # xwalk
-    # postprocess
-    return df
 
-def build_bird_detection() -> pd.DataFrame:
-    # connect to db
-    con = dbc._db_connect('access')
-    # execute qry against db
-    df = dbc._exec_qry(con=con, qry='get_tbl_field_data')
-    # close connection
-    con.close()
-    # preprocess
-    # xwalk
-    # postprocess
-    return df
+    return template_dict
 
-def _get_tbls(tbls:list=[], tbl_list:list=tbl_list) -> dict:
+def _get_src_tbls(tbl_list:list=tbl_list) -> dict:
+    """Make empty dataframes with the correct column names and order for each table to be loaded
 
-    if len(tbls)==0:
-        tbls = tbl_list.copy() # base case: query all tables
-    else:
-        no_tbls = [x for x in tbls if x not in tbl_list]
-        tbls = [x for x in tbls if x in tbl_list]
-        if len(no_tbls)>0:
-            for tbl in no_tbls:
-                print(f'You provided {tbl} which is not a valid table name.')
+    Args:
+        dest (str, optional): Absolute or relative filepath where you want to save the dictionary of dataframes
+
+    Returns:
+        dict: Dictionary of dataframes
+
+    Examples:
+        import src.make_templates as bt
+        testdict = bt._get_src_tbls()
+        with open('saved_dictionary.pkl', 'rb') as f:
+            loaded_dict = pickle.load(f)
+    """
     # connect to db
     con = dbc._db_connect('access')
     tbl_dict = {}
-    for tbl in tbls:
+    for tbl in tbl_list:
         tbl_dict[tbl] = dbc._exec_qry(con=con, qry=f'get_{tbl}')
 
     # close connection
