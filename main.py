4b6cf6d3f5a262891f1b630fd3359e1796c4520a
cwainright-nps <charles_wainright@nps.gov>
2024-02-14 14:39:29 -0500

    [feature] added error checking to main workflow

--- main.py
+++ main.py
@@ -8,8 +8,11 @@ WORKFLOW -- repeat these steps for each destination table to scale up
 3. add xwalk function to src.tbl_xwalks.py
     e.g., _detection_event_xwalk()
 4. add a function call to src.make_templates.create_xwalks() for the function you wrote in step 3
+5. Add details to xwalk in src.tbl_xwalks.py function
+    e.g., _detection_event_xwalk()
 """
 
 import src.make_templates as mt
-
+import src.check as c
 testdict = mt.make_birds()
+c.check_birds(testdict)
