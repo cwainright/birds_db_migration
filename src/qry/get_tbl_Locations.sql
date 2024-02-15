aa87fbf9c901c09f18230e589e132b334d017604
cwainright-nps <charles_wainright@nps.gov>
2024-02-15 10:41:45 -0500

    [bug fix] there each `tbl_Sites` and `tbl_Locations` had a field called `Unit_Code` that had different values, which made the `source` dataframe difficult to resolve. Resolved this bug by removing `tbl_Sites.Unit_Code` from the SELECT statement.

--- src/qry/get_tbl_Locations.sql
+++ src/qry/get_tbl_Locations.sql
@@ -1,1 +1,2 @@
-SELECT * FROM tbl_Locations LEFT JOIN tbl_Sites ON tbl_Locations.Site_ID = tbl_Sites.Site_ID;
+SELECT tbl_Locations.*, tbl_Sites.Site_ID, tbl_Sites.GIS_Location_ID, tbl_Sites.Site_Name, tbl_Sites.Site_Desc, tbl_Sites.Site_Notes, tbl_Sites.Route_Type
+FROM tbl_Locations LEFT JOIN tbl_Sites ON tbl_Sites.Site_ID = tbl_Locations.Site_ID;
