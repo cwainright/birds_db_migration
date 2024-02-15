cb69d79f7362ef0c28fad885401ce2c98a291a16
cwainright-nps <charles_wainright@nps.gov>
2024-02-15 16:03:36 -0500

    [bugfix] query was returning a duplicate column because dbo_tlu_Guild_Assignment.Guild_ID = dbo_tbl_Guilds.Guild_ID

--- src/qry/get_dbo_tlu_GuildAssignment.sql
+++ src/qry/get_dbo_tlu_GuildAssignment.sql
@@ -1,1 +1,2 @@
-SELECT * FROM dbo_tlu_Guild_Assignment LEFT JOIN dbo_tbl_Guilds ON dbo_tlu_Guild_Assignment.Guild_ID = dbo_tbl_Guilds.Guild_ID;
+SELECT dbo_tlu_Guild_Assignment.*, dbo_tbl_Guilds.Integrity_Element, dbo_tbl_Guilds.Integrity_Element, dbo_tbl_Guilds.Guild_Name, dbo_tbl_Guilds.Guild_Desc, dbo_tbl_Guilds.GuildCategory, *
+FROM dbo_tlu_Guild_Assignment LEFT JOIN dbo_tbl_Guilds ON dbo_tlu_Guild_Assignment.Guild_ID = dbo_tbl_Guilds.Guild_ID;
