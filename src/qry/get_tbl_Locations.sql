e17c2ce4ca6f19ec2cf8ef540f7a95bb48b75e8d
cwainright-nps <charles_wainright@nps.gov>
2024-02-20 12:41:19 -0500

    [debug] stopped joining tbl_Sites to tbl_Locations because tbl_Sites has its own destination table

--- src/qry/get_tbl_Locations.sql
+++ src/qry/get_tbl_Locations.sql
@@ -1,2 +1,1 @@
-SELECT tbl_Locations.*, tbl_Sites.GIS_Location_ID, tbl_Sites.Site_Name, tbl_Sites.Site_Desc, tbl_Sites.Site_Notes, tbl_Sites.Route_Type
-FROM tbl_Locations LEFT JOIN tbl_Sites ON tbl_Sites.Site_ID = tbl_Locations.Site_ID;
+SELECT * FROM tbl_Locations;
