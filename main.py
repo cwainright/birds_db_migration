6d996be9a97e7521ab2b112e0d8cd5e350bd94fe
cwainright-nps <charles_wainright@nps.gov>
2024-04-05 12:02:07 -0400

    [feature] added database loading to main workflow

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-"""This is the main entry-point into the program"""
+"""This is the main workflow for building `birds` from-source, loading to database, and validating each step"""
 
 import src.make_templates as mt
 import src.check as c
+import src.load_tbls as loader
 
 if __name__ == '__main__':
     birds = mt.make_birds()
@@ -9,5 +10,8 @@ if __name__ == '__main__':
     n = input('How many records would you like to unit test? (Integer required).')
     verbose = input('Do you want to print the results of the unit test to console? (True or False required).')
     c.unit_test(birds, n, verbose)
+    permission = input('Do you want to load birds to db now? y/n')
+    if permission.lower() == 'y':
+        loader.load_tbl(birds)
     birds = c.validate_db(birds, n, verbose)
     
